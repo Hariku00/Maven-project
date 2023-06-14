7d0525295aba49725871d35868f0999ba7134707
hari <hari@DESKTOP-PFNBBQU>
2023-06-14 12:02:33 +0530

    OBSQUARA

---
+++ Maven_Project/src/main/java/parent/Child.java
@@ -0,0 +1,17 @@
+package parent;
+
+public class Child {
+	public void add()
+	{
+		System.out.println("hari");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child a=new Child();
+		a.add();
+
+	}
+
+}
