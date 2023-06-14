a6cb643b43b734fc97215751530728d350d8ff40
hari <hari@DESKTOP-PFNBBQU>
2023-06-14 12:10:27 +0530

    OBSQUARA

--- Maven_Project/src/main/java/parent/Child.java
+++ Maven_Project/src/main/java/parent/Child.java
@@ -5,12 +5,18 @@ public class Child {
 	{
 		System.out.println("hari");
 	}
+	public void print()
+	{
+		System.out.println("krishnan");
+		
+	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Child a=new Child();
 		a.add();
+		a.print();
 
 	}
 
